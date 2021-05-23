90bf03935c173ce5dabda5437c92030a68f6deec
SahanMonaara <sahan.alahakoon@interblocks.com>
2021-05-24 00:52:43 +0530

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by Sahan Alahakoon on 5/24/21.
+//  Copyright © 2021 Sahan Alahakoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
